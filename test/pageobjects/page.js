3f4da0c6791f248961134c1cd7cce7b32969847c
Zarahi93 <adahi.g.z@hotmail.com>
2024-05-27 23:14:14 -0600

    Including wdio conf

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -9,8 +9,8 @@ export default class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open (path) {
-        return browser.url(`https://pastebin.com/`)
+    async open (path) {
+        await browser.url('https://pastebin.com/');
     };
 
     get textAreaId(){
--- test/specs/test.js
+++ test/specs/test.js
@@ -1,7 +1,8 @@
-import Page from '../pageobjects/page';
+import Page from '../pageobjects/page.js';
 
 describe('Create New Paste', () => {
-    it('should create a new paste with the following attributes', async () => {
+
+    it('should create a new paste', async () => {
         await browser.deleteCookies()
         await Page.open();
         await Page.newPaste('Hello from WebDriver', 'helloweb');
