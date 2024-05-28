4ae4512b6f5097e2cf959fbd9818d32d598deef7
Zarahi93 <adahi.g.z@hotmail.com>
2024-05-27 21:13:52 -0600

    name change

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,6 +2,7 @@ import Page from '../pageobjects/page';
 
 describe('Create New Paste', () => {
     it('should create a new paste with the following attributes', async () => {
+        await browser.deleteCookies()
         await Page.open();
         await Page.newPaste('Hello from WebDriver', 'helloweb');
     })
